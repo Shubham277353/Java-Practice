6b23f7acd44f6972279cb0203e1e25dd665ce84b
Shubham277353 <golubhandari405@gmail.com>
2025-08-05 16:39:46 +0530

    update: practice java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.sql.SQLOutput;
+
+public class Main{
+    public static void main(String[] args){
+    int[][] myNumbers = {{1,3,5,7},{2,4,6,8}};
+        System.out.println(myNumbers[0][3]);
+    }
+}
