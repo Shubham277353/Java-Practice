b4acedb39dcdcb1733350fa147dd1ab17abe3943
Shubham277353 <golubhandari405@gmail.com>
2025-08-05 17:00:32 +0530

    practice java methods

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.sql.SQLOutput;
 public class Main{
     public static void main(String[] args){
     int[][] myNumbers = {{1,3,5,7},{2,4,6,8}};
-        System.out.println(myNumbers[0][3]);
+        for (int[] myNumber : myNumbers) {
+            for (int j = 0; j < myNumber.length; j++) {
+                System.out.println(myNumber[j]);
+            }
+        }
     }
 }
---
+++ src/Methods.java
@@ -0,0 +1,10 @@
+public class Methods {
+    static void greetings(String name , int age){
+        System.out.println("Hello " + name + "!, you are "+age+" aren't you ? ");
+
+    }
+    public static void main(String[] args){
+        greetings("Shubham" , 21);
+
+    }
+}
