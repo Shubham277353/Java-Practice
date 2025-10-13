21a9b795e25ec66610b358b650c1d2030ea665b0
Shubham277353 <golubhandari405@gmail.com>
2025-10-13 23:14:35 +0530

    second question

--- src/KunalKushwahaAssignmentsQuestions.java
+++ src/KunalKushwahaAssignmentsQuestions.java
@@ -2,18 +2,14 @@ import java.util.Scanner;
 
 public class KunalKushwahaAssignmentsQuestions {
     public static void main(String[] args) {
-        // Write a program to print whether a number is even or odd, also take input from the user.
+        //Take name as input and print a greeting message for that particular name.
         Scanner in = new Scanner(System.in);
 
         for (int i = 0; i <= 10; i++) {
-            System.out.print("Enter the number: ");
-            int num = in.nextInt();
+            System.out.print("Enter your name : ");
+            String name = in.nextLine();
 
-            if (num % 2 == 0) {
-                System.out.println(num + " is an even number.");
-            } else {
-                System.out.println(num + " is an odd number");
-            }
+            System.out.println("Good morning, "+ name + "!");
 
         }
     }
