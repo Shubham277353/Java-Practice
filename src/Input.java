3f3ed08c2172edf1622ddd34a83b8a46715db918
Shubham277353 <golubhandari405@gmail.com>
2025-10-13 23:12:06 +0530

    solve 1st question of kunal's assignment.

--- src/Input.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Input {
-    public static void main (String[] args){
-        Scanner input = new Scanner(System.in);
-        String rollNo = input.nextLine();
-        System.out.println(rollNo);
-    }
-}
