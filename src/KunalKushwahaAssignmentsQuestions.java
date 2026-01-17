51e472f57a81612dd0b9185a1bcc47782e558b9e
Shubham277353 <golubhandari405@gmail.com>
2026-01-17 09:48:33 +0530

    practice java functions

--- src/KunalKushwahaAssignmentsQuestions.java
+++ src/KunalKushwahaAssignmentsQuestions.java
@@ -1,18 +1,9 @@
 import java.util.Scanner;
 
-public class KunalKushwahaAssignmentsQuestions {
-    public static void main(String[] args) {
-        //Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
-        Scanner in = new Scanner(System.in);
+public class
 
-        System.out.print("Enter the principal amount : ");
-        double principal = in.nextDouble();
-        System.out.print("Enter the time (in years): ");
-        double time = in.nextDouble();
-        System.out.print("Enter the rate : ");
-        double rate = in.nextDouble();
-
-        double simpleInterest = (principal*time*rate)/100;
-        System.out.println("Your simple interst is " + simpleInterest);
-    }
+public void swapNumbers(num1,num2){
+    int temp = num1;
+    int num1 = num2;
+    int num2 = temp;
 }
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 
 public class Main{
 
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
+    public static void swapNumbers(num1,num2){
+        int temp = num1;
+        int num1 = num2;
+        int num2 = temp;
+    }
 
-        System.out.print("Enter first number: ");
-        int num1 = input.nextInt();
-        System.out.print("Enter second number: ");
-        int num2 = input.nextInt();
-
-        int sum = num1 + num2;
-
-        System.out.println("The sum is " +sum+".");
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 11;
+        swapNumbers(num1,num2);
     }
 }
